c1f51b9877f5fbdc150f67f5763e00b15e82a93b
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2021-10-05 15:44:58 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function deer() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "https://i.kym-cdn.com/photos/images/newsfeed/001/959/908/67c.gif");
+    x.setAttribute("alt", "Deer?");
+    document.body.appendChild(x);
+}
